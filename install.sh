68c59f2c9f349b633b4c942cfc7abcb5486e4fcd
Melody <machineonamission@gmail.com>
2026-03-01 13:13:45 -0600

    this is probably fine

--- install.sh
+++ install.sh
@@ -1,8 +1,8 @@
 git pull
-sleep 1
-systemctl stop smarthome-pod.service
+#sleep 1
+#systemctl stop smarthome-pod.service
 sleep 1
 find ./quadlet -type f -exec cp {} /etc/containers/systemd/ \;
 sleep 1
 systemctl daemon-reload
-systemctl restart smarthome-pod.service
+#systemctl restart smarthome-pod.service
