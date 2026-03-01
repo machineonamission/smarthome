64d6304eaebccb5737baa031f715e9bf2cd625f0
Melody <machineonamission@gmail.com>
2026-02-28 21:46:42 -0600

    fgfbgbgbgbgb

--- install.sh
+++ install.sh
@@ -1,5 +1,8 @@
 git pull
 sleep 1
-find ./quadlet -type f -exec podman quadlet install -r {} +
+systemctl stop smarthome-pod.service
 sleep 1
+find ./quadlet -type f -exec podman quadlet install --reload-systemd -r {} +
+sleep 1
+systemctl daemon-reload
 systemctl restart smarthome-pod.service
