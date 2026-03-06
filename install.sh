e9cf5394aec3d28105c7078c1e6126d832ab8e52
Melody <machineonamission@gmail.com>
2026-03-05 23:50:20 -0600

    CORTEX!

--- install.sh
+++ install.sh
@@ -1,8 +1,8 @@
 git pull
 #sleep 1
 #systemctl stop smarthome-pod.service
-sleep 1
+sudo -u melody cp -r ./pipewire /home/melody/.config/pipewire/pipewire.conf.d/
 find ./quadlet -type f -exec cp {} /etc/containers/systemd/ \;
-sleep 1
+sleep 2
 systemctl daemon-reload
 #systemctl restart smarthome-pod.service
